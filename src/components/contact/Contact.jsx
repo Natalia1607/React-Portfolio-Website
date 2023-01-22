8c246fb50cd5dd464e6be2d453614838b6542be1
Natalia Gilyazova <gilyazovan97@mail.ru>
2023-01-22 18:01:11 +0300

    Created sections portfolio and contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import './contactStyles.css';
+import {MdOutlineEmail} from 'react-icons/md';
+import {FaTelegramPlane} from 'react-icons/fa';
 
 const Contact = () => {
   return (
     <section id='contact'>
-      
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>gilyazovan97@mail.ru</h5>
+            <a href="mailto:gilyazovan97@mail.ru" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <FaTelegramPlane className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Telegram</h5>
+            <a href="https://t.me/natalia_ksno" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        <form action="#">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea type="message" rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
     </section>
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
 import './portfolioStyles.css';
+import IMG1 from '../../assets/portfolio1.jpg';
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Title',
+    github: 'https://github.com/Natalia1607',
+    demo: 'https://github.com/Natalia1607'
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Title2',
+    github: 'https://github.com/Natalia1607',
+    demo: 'https://github.com/Natalia1607'
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Title3',
+    github: 'https://github.com/Natalia1607',
+    demo: 'https://github.com/Natalia1607'
+  },
+];
 
 const Portfolio = () => {
-  return (
-    <section id='portfolio'>Portfolio</section>
+  return ( 
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article className="portfolio__item" key={id}>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
