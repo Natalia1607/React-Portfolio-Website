76f40f909b1e1eec4003825dd15b41f3b2667781
Natalia Gilyazova <gilyazovan97@mail.ru>
2023-01-23 12:44:36 +0300

    Created footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,22 @@ import './footerStyles.css';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer> 
+      <a href="#"className='footer__logo'>EGATOR</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className="footer__copyright">
+        <small>&copy; Gilyazova Natalia. All right reserverd.</small>
+      </div>
+    </footer>
   )
 }
 
